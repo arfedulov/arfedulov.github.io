d2288bcf746ef0c8c584cc3a794b05fb0fb99de5
Artem Fedulov <arfedulov@gmail.com>
2021-09-24 23:41:38 +0300

    Edit: add offline access

---
+++ edit/precache-manifest.68949ea815bca41e52e5e3dbbbf7c22a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d3252799f5c53717a2",
+    "url": "/edit/css/app.dc32ca6c.css"
+  },
+  {
+    "revision": "9c947590299364252727ca1d7ae19909",
+    "url": "/edit/favicon.png"
+  },
+  {
+    "revision": "0bc78d933e08f476c1fe5fda110b8469",
+    "url": "/edit/index.html"
+  },
+  {
+    "revision": "46d3252799f5c53717a2",
+    "url": "/edit/js/app.2d48f908.js"
+  },
+  {
+    "revision": "ad07c6acca0c57496ff0",
+    "url": "/edit/js/chunk-vendors.43c1d6dd.js"
+  },
+  {
+    "revision": "674e5f13ebf4303c65d3c3ba3bb253a0",
+    "url": "/edit/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/edit/robots.txt"
+  }
+]);
